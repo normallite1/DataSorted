d9bf8d542a085bb06341abedca0eb8a9bb9a1477
normallite1 <normallite@gmail.com>
2022-11-17 00:19:39 +0200

    change char "r" on "R"))

--- src/Impl/DataSortedService.java
+++ src/Impl/DataSortedService.java
@@ -5,21 +5,20 @@ import Interface.IDateSorter;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static java.util.Collections.reverseOrder;
-import static java.util.Collections.sort;
+
 
 public class DataSortedService implements IDateSorter{
     @Override
     public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
         List<LocalDate> sortDataWithR = unsortedDates.stream()
-                .filter(a -> !a.getMonth().toString().contains("r"))
+                .filter(a->a.getMonth().toString().contains("R"))
                 .sorted()
-                .collect(Collectors.toCollection(ArrayList::new));
+                .collect(Collectors.toList());
 
         sortDataWithR.addAll(unsortedDates.stream()
-                .filter(a -> a.getMonth().toString().contains("r"))
+                .filter(a -> !a.getMonth().name().contains("R"))
                 .sorted(reverseOrder()).toList());
 
 
--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,14 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<LocalDate> dateList = Arrays.asList(
-                LocalDate.of(2005, 7, 01),
-                LocalDate.of(2005, 01, 02),
-                LocalDate.of(2005,01,01),
-                LocalDate.of(2005,05,03));
+                LocalDate.of(2005, 7, 1),
+                LocalDate.of(2005, 1, 2),
+                LocalDate.of(2005,1,1),
+                LocalDate.of(2005,5,3));
         DataSortedService dataSortedService = new DataSortedService();
         System.out.println("After: " +dateList);
-        Collection<LocalDate> sortedDataList = dataSortedService.sortDates(dateList);
-        System.out.println("Before: " +sortedDataList);
+        System.out.println("Before: " +dataSortedService.sortDates(dateList));
+
 
     }
 }
